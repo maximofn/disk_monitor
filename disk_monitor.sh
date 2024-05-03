56c73e6305ef9260430f8309b0ccbbe22ed0baea
maximofn <maximofn@gmail.com>
2024-05-03 09:19:58 +0200

    Create logs files

--- disk_monitor.sh
+++ disk_monitor.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # Get script path
 SCRIPT_PATH=$(dirname $0)
-/usr/bin/python3 $SCRIPT_PATH/disk_monitor.py
+/usr/bin/python3 $SCRIPT_PATH/disk_monitor.py >disk_monitor.log 2>disk_monitor_error.log
